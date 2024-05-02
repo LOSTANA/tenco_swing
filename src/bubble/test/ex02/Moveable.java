c3ec7ecfb9372d2013b5929833a5fef778b02ee9
LOSTANA <jyj970602@naver.com>
2024-05-02 15:06:10 +0900

    5월 2일 2차

---
+++ src/bubble/test/ex02/Moveable.java
@@ -0,0 +1,13 @@
+package bubble.test.ex02;
+
+public interface Moveable {
+
+	
+	//public abstract 생략가능
+	public abstract void left();
+	public abstract void right();
+	public abstract void up();
+	public abstract void down();
+	
+	
+}
